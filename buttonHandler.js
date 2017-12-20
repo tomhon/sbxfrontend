8a10f2867780b33b3732833f5a9f450d631ccac1
Tom Honeybone <tomhon@hotmail.com>
2017-12-20 15:33:54 -0800

    Updates to button handling

--- buttonHandler.js
+++ buttonHandler.js
@@ -1,5 +1,18 @@
 function updateLaneColor(courseLocation, laneSelected, colorSelected) {
     // colorSelected = document.getElementById(laneSelected).value;
     console.log(courseLocation, laneSelected, colorSelected);
-    document.getElementById("dropbtnLane1").style.backgroundcolor = "red";
+    refreshLaneButtonBackgrounds(courseLocation);
+    // start.laneSelected = colorSelected;
+    document.getElementById(laneSelected).style.backgroundColor = colorSelected;
+    
 };
+
+
+function refreshLaneButtonBackgrounds(courseLocation) {
+    document.getElementById("buttonLane1").style.backgroundColor = start.buttonLane1;
+    document.getElementById("buttonLane2").style.backgroundColor = start.buttonLane2;
+    document.getElementById("buttonLane3").style.backgroundColor = start.buttonLane3;
+    document.getElementById("buttonLane4").style.backgroundColor = start.buttonLane4;
+    document.getElementById("buttonLane5").style.backgroundColor = start.buttonLane5;
+    document.getElementById("buttonLane6").style.backgroundColor = start.buttonLane6;
+}
---
+++ initializeDataStructures.js
@@ -0,0 +1,8 @@
+var start = {
+    'buttonLane1' : "teal",
+    'buttonLane2' : "teal",
+    'buttonLane3' : "teal",
+    'buttonLane4' : "teal",
+    'buttonLane5' : "teal",
+    'buttonLane6' : "teal",
+}
